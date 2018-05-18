3bc736b933c8d28a074f50b19455a3ad697d48c8
Peter Kiraly <zsonglorman@gmail.com>
2018-05-18 01:46:35 -0700

    Fix unit tests with mock

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,45 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  TorpedoStore primary;
+  TorpedoStore secondary;
+
   @Before
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primary, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(1)).fire(1);
   }
 
 }
